6021863e843f7e38d84286b66e8112a0dc0f25d9
Niladri Basak <niladri.basak.007@gmail.com>
2023-07-24 16:56:47 +0530

    Update script.js

--- script.js
+++ script.js
@@ -1,5 +1,37 @@
-var settingsmenu = document.querySelector(".settings-menu");
+var settingsMenu = document.querySelector(".settings-menu");
+var darkBtn = document.getElementById("dark-btn");
 
-function settingsMenuToggle(){
-    settingsmenu.classList.toggle("settings-menu-height");
+function settingsMenuToggle() {
+    settingsMenu.classList.toggle("settings-menu-height");
 }
+
+darkBtn.onclick = function () {
+    darkBtn.classList.toggle("dark-btn-on");
+    document.body.classList.toggle("dark-theme");
+
+    if (localStorage.getItem("theme") == "light") {
+        localStorage.setItem("theme", "dark");
+    }
+    else {
+        localStorage.setItem("theme", "light");
+    }
+
+}
+
+// local storage
+
+if (localStorage.getItem("theme") == "light") {
+    darkBtn.classList.remove("dark-btn-on");
+    document.body.classList.remove("dark-theme");
+}
+else if (localStorage.getItem("theme") == "dark") {
+    darkBtn.classList.add("dark-btn-on");
+    document.body.classList.add("dark-theme");
+}
+else {
+    localStorage.setItem("theme", "light");
+}
+
+
+// localStorage.setItem("theme", "light"); // local storage name is theme and the value is dark
+// localStorage.getItem("theme"); // this gives the value stored in local storage theme
